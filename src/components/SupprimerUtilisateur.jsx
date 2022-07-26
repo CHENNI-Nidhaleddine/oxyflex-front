9ad3421ca7798e21bc728d94047f48e55d4aff1e
Amina <ziad.amina.nourhane@gmail.com>
2022-07-26 16:17:39 +0200

    utilisateurs ui

---
+++ src/components/SupprimerUtilisateur.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {
+  Button,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function SupprimerUtilisateur() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+
+  return (
+    <>
+      <Button
+        flex={1}
+        onClick={onOpen}
+        fontSize={"sm"}
+        rounded={"full"}
+        bg={"blue.400"}
+        color={"white"}
+        boxShadow={
+          "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+        }
+        _hover={{
+          bg: "blue.500",
+        }}
+        _focus={{
+          bg: "blue.500",
+        }}
+      >
+        Supprimer
+      </Button>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Supprimer utilisateur
+            </AlertDialogHeader>
+
+            <AlertDialogBody>Êtes-vous sûr?</AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Supprimer
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
+
+export default SupprimerUtilisateur;
--- src/pages/Tabs/Notifications.jsx
+++ src/pages/Tabs/Notifications.jsx
@@ -1,8 +1,71 @@
-function Notifications(){
-    return(
-        <div>
-            Notifications
+import { ImBell } from "react-icons/im";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import { border, color } from "@mui/system";
+
+function Notifications() {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <div className="shadow-xl w-2/3 font-bold text-3xl bg-[#fff] rounded flex items-center justify-between">
+        <div className="flex items-center justify-center">
+          <div className="mr-5 bg-[#2d62ec] text-[#fff] text-5xl p-2 rounded-l">
+            <ImBell />
+          </div>
+          <h1>Notification</h1>
         </div>
-    )
+      </div>
+
+      <div
+        className="shadow-xl flex flex-col items-center justify-between bg-[#fff] rounded-xl w-1/3 h-fit py-5 mt-16 border-t-8 border-[#2d62ec]"
+        style={{ display: "flex", flexWrap: "wrap" }}
+      >
+        <div className=" flex items-center justify-between px-10 my-2 text-20px font-bold">
+          Liste des services atteindraient la quantité min d'usage
+          <Button bg={"#2d62ec"}>30%</Button>
+        </div>
+
+        <div
+          style={{
+            backgroundColor: "#EEECF9",
+            borderRadius: "10px",
+            border: "1px solid #EEECF9",
+          }}
+        >
+          <text align="left">Cardiologie </text>
+
+          <text align="right">23% </text>
+          <Text fontSize="2xl" color="#F65F9C   ">
+            17 Bouteilles
+          </Text>
+        </div>
+        <div
+          style={{
+            backgroundColor: "#EEECF9",
+            borderRadius: "10px",
+            border: "1px solid #EEECF9",
+          }}
+        >
+          <text align="left">Cardiologie </text>
+
+          <text align="right">23% </text>
+          <Text fontSize="2xl" color="#F65F9C   ">
+            17 Bouteilles
+          </Text>
+        </div>
+        <div id="textbox">
+          <p class="alignleft">Min stock:</p>
+          <p class="alignright">20%.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Notifications;
